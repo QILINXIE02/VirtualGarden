63c80001d8a0e5d50cac593ea4a3770684af6810
qilinxie02 <qxie@gicw.org>
2024-08-26 08:32:46 -0700

    fixes app entry point

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
-import GardenScreen from './src/screens/GardenScreen';
+import GardenScreen from './src/screens/PlantDetails';
 
 const Stack = createStackNavigator();
 
